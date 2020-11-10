06842ede0553a6901ae8f4994527287e43b54f7c
DingWB <ding_wu_bin@163.com>
2020-11-10 20:01:41 +0800

    remove utility

--- tbmate/tbmate.py
+++ tbmate/tbmate.py
@@ -11,7 +11,63 @@ import struct
 import tabix
 import gzip
 import pandas as pd
-from utility import *
+
+version=1.0
+
+dtype_fmt={
+        'float':'f',
+        'int':'i',
+        'string':'s',
+        'chr':'c',
+        'double':'d'
+        }
+
+dtype_map={
+        'int':1,
+        'float':4,
+        'double':5,
+        'string':7,
+        'chr':6
+        }
+
+dtype_map_rev={
+        1:'int',
+        2:'int',
+        3:'int',
+        4:'float',
+        5:'double',
+        6:'chr',
+        7:'string'
+        }
+
+dtype_func={
+        'float':float,
+        'int':int,
+        'string':str,
+        'chr':str,
+        'double':float
+        }
+
+def pack_header(idx,outfile,dtype):
+    print(f"Packing and writing to {outfile}")
+    idx_len=len(idx)
+    fo=open(outfile,'wb')
+    fo.write(struct.pack('3s',b'tbk')) #'tbk',byte=3*1=3
+    fo.write(struct.pack('f',version)) #version:1; byte=4
+    fo.write(struct.pack('q',dtype_map[dtype])) #dtype, bytes=8
+#    fo.write(struct.pack('i',idx_len)) #idx_len, byte=4
+    fo.write(struct.pack('q',-1)) #max data, byte=8
+
+    if idx_len > 8169:
+        idx=idx[:8169]
+
+    fo.write(struct.pack(f'{idx_len}s',bytes(idx,'utf-8'))) #idx file, byte=idx_len
+
+    if idx_len < 8169:
+        for i in range(8169-idx_len):
+            fo.write(struct.pack('x')) #fill to 500.
+    #Total used bytes = 3+4+8+8+8169=8192
+    return fo
 # =============================================================================
 def Pack(Input,idx='idx.gz',basename="out",cols_to_pack=[4],
         dtypes=['float']):
@@ -88,7 +144,6 @@ def pack_list(L=None,idx='idx.gz',dtype='float',outfile="out.tbk"):
     f.seek(15)
     f.write(struct.pack('q',num))
     f.close()
-    
 # =============================================================================
 def to_tbk(data=None,cols=[],idx='idx.gz',outdir="./",
         dtypes=[],na=-1):
@@ -123,7 +178,6 @@ def to_tbk(data=None,cols=[],idx='idx.gz',outdir="./",
         df_idx[col].fillna(na,inplace=True)
         pack_list(L=df_idx[col].tolist(),idx=idx,dtype=dtype,\
                   outfile=os.path.join(outdir,col+'.tbk'))
-    
 # =============================================================================
 def Read(tbk_file,start,size,fmt):
     """
