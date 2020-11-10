777c6ee5e83df8f02b28966b84324c7d0651e951
DingWB <ding_wu_bin@163.com>
2020-11-10 20:45:11 +0800

    remove blank line

--- tbmate/__init__.py
+++ tbmate/__init__.py
@@ -7,6 +7,5 @@ Created on Sun Nov  8 14:52:41 2020
 """
 from .tbmate import pack_list,to_tbk,Read,Header,Query
 from .tbmate import read_one_site,read_multi_samples,ReadBulk
-
 __all__=['tbmate']
 __version__='1.0'
