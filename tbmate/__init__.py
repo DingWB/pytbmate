a35f4ce4625763c199ffedd328eba641f25ece32
DingWB <ding_wu_bin@163.com>
2020-11-10 20:28:02 +0800

    Update __init__.py

--- tbmate/__init__.py
+++ tbmate/__init__.py
@@ -6,6 +6,7 @@ Created on Sun Nov  8 14:52:41 2020
 @author: DingWB
 """
 
-from .tbmate import *
 __all__=['tbmate']
 __version__='1.0'
+from .tbmate import pack_list,to_tbk,Read,Header,Query
+from .tbmate import read_one_site,read_multi_samples,ReadBulk
